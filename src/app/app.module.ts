eee2e2b953e25eead9d526a00b5cbeb4ac61179f
Shireesh Kumar <g.shireesh.kumar@gmail.com>
2018-06-21 18:10:31 +0530

    ngsw worker path fix.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,7 @@ import { environment } from '../environments/environment';
   imports: [
     BrowserModule,
     NgxInfiniteVirtualScrollModule,
-    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register('./ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
